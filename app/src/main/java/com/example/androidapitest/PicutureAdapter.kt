3203476ad5e6d2262bbdc63b3d817a49f1392c1e
shanpu <cheztomo513@gmail.com>
2018-06-10 13:01:25 +0900

    サーバから画像取得機能を追加
    とりあえずデータベースは使わない方向で都度サーバから取得しています

--- app/src/main/java/com/example/androidapitest/PicutureAdapter.kt
+++ app/src/main/java/com/example/androidapitest/PicutureAdapter.kt
@@ -1,71 +1,24 @@
 package com.example.androidapitest
 
 import android.content.Context
-import android.net.Uri
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import android.widget.ImageView
-import android.widget.TextView
-import com.bumptech.glide.Glide
-import com.example.androidapitest.R.layout.grid_items
-import io.realm.OrderedRealmCollection
-import io.realm.RealmBaseAdapter
-import java.text.DateFormat
-import java.util.*
+import com.example.androidapitest.model.Picture
+import com.example.androidapitest.view.PictureView
 
-class PicutureAdapter(layout: Int, names: MutableList<String>, dates: MutableList<Date>, pictures: MutableList<String>) : BaseAdapter(){
+class PicutureAdapter(private val context: Context) : BaseAdapter(){
 
-    val layout = layout
-    val names: MutableList<String> = names
-    val dates: MutableList<Date> = dates
-    val pictures: MutableList<String> = pictures
+    var pictures: List<Picture> = emptyList()
 
-    inner class ViewHolder(cell: View) {
-        val date = cell.findViewById<TextView>(R.id.date)
-        val name = cell.findViewById<TextView>(R.id.name)
-        val picture = cell.findViewById<ImageView>(R.id.imageView)
-    }
+    override fun getCount(): Int = pictures.size
 
+    override fun getItem(position: Int): Any? = pictures[position]
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+    override fun getItemId(position: Int): Long = 0
 
-        val view: View
-        val viewHolder: ViewHolder
-
-        when (convertView) {
-            null-> {
-                val inflater = LayoutInflater.from(parent?.context)
-                view = inflater.inflate(R.layout.grid_items, parent, false)
-                viewHolder = ViewHolder(view)
-                view.tag = viewHolder
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View =
+            ((convertView as? PictureView) ?: PictureView(context)).apply {
+                setPicture(pictures[position])
             }
-            else -> {
-                view = convertView
-                viewHolder = view.tag as ViewHolder
-            }
-        }
-
-        viewHolder.date.text = android.text.format.DateFormat.format("yyyy/MM/dd", dates[position])
-        viewHolder.name.text = names[position]
-        Glide.with(viewHolder.picture).load(pictures[position]).into(viewHolder.picture)
-
-        return view
-    }
-
-    override fun getItem(position: Int): Any? {
-        return null
-    }
-
-    override fun getItemId(position: Int): Long {
-        return 0
-    }
-
-    override fun getCount(): Int {
-        return names.size
-    }
-
-
-
 }
--- app/src/main/java/com/example/androidapitest/client/PictureClient.kt
+++ app/src/main/java/com/example/androidapitest/client/PictureClient.kt
@@ -1,12 +1,11 @@
 package com.example.androidapitest.client
 
-import com.example.androidapitest.model.Picture
+import com.example.androidapitest.model.ImageList
 import retrofit2.http.GET
-import retrofit2.http.Query
 import rx.Observable
 
 interface PictureClient {
 
-    @GET("/api/v1/images/")
-    fun serch(@Query("query") query: String): Observable<List<Picture>>
+    @GET("/api/v1/images")
+    fun search(): Observable<ImageList>
 }
